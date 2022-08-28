5e0ac06d6eaf64c0bf30cd3ac47e7e65add6ced5
Azis Ramadhan <git@azram.eu.org>
2022-08-28 19:50:03 +0700

    Feature : Remove Item & Checklist item

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
+// ↓↓↓ WARNING: HEADACHE AHEAD ↓↓↓ //
+
 const vh = window.innerHeight
 const todoItem = document.querySelector('.todoItem')
-const html = todoItem.outerHTML
+const insertLoc = document.querySelector('.todoInsert')
+const outerTodo = todoItem.outerHTML
+const removeButton = document.getElementsByClassName('rmv')
+let i
 
 document.documentElement.style.setProperty('--vh', `${vh}px`)
 
 function addTodo() {
-  todoItem.insertAdjacentHTML("afterend", html)
+  insertLoc.insertAdjacentHTML("beforebegin", outerTodo)
+  addRemoveFunc()
 }
 
-function rmvTodo() {
-
+function addRemoveFunc() {
+  for (i = 0; i < removeButton.length; i++) {
+    removeButton[i].onclick = function() {
+      this.parentElement.remove()
+    }
+  }
 }
+addRemoveFunc() //Allow for first todo item to be removed
+
+function checklist(event) {
+  event.target.nextElementSibling.classList.toggle('checked')
+}
+
+window.onload = function() {
+  document.querySelector('.textbox').value = ''
+  document.getElementById('checkbox').checked = false
+} // Prevent checked state becoming true before clicked or idk, try to remove and see for yourself
